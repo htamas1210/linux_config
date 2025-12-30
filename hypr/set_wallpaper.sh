b1b5c0ae7ca290a66ec2eef8c0fb034c19276a6f
htom <hatvanitamas09@gmail.com>
2025-12-30 18:54:38 +0100

    fixed hyprpaper with the new update

--- hypr/set_wallpaper.sh
+++ hypr/set_wallpaper.sh
@@ -5,4 +5,6 @@ WALLPAPER_DIR="$HOME/Pictures/Wallpaper/"
 CURRENT_WALLPAPERS=$(hyprctl hyprpaper listloaded)
 
 WALLPAPER=$(find "$WALLPAPER_DIR" -type f ! -name "$(basename "$CURRENT_WALLPAPER")" | grep -v .git | shuf -n 1)
-hyprctl hyprpaper reload ,"$WALLPAPER"
+echo $WALLPAPER
+hyprctl hyprpaper wallpaper "eDP-1, $WALLPAPER ,"
+hyprctl hyprpaper wallpaper "HDMI-A-1, $WALLPAPER ,"
